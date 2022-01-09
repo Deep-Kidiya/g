9b87a31039a2f262c85eaa9c72ce70c9dc1d0f95
Deep Kidiya <95274924+Deep-Kidiya@users.noreply.github.com>
2022-01-09 13:54:26 +0530

    reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,25 @@ $(window).on("load", function () {
   $(".loader-wrap").fadeOut(3000);
 });
 // ================================= end::loader =============================
+
+// ================================= begin::scroll reveal =============================
+window.addEventListener('scroll', reveal);
+
+// function reveal() {
+//   var reveal = document.querySelectorAll('.reveal');
+
+//   for (var i = 0; i < reveal.length; i++) {
+    
+//     var windowheight = window.innerHeight;
+//     var revealtop = reveal[i].getBoundingClientRect().top;
+//     var revealpoint = 150;
+
+//     if (revealtop < windowheight - windowheight) {
+//       reveal[i].classList.add('active');
+//     } else {
+//       reveal[i].classList.remove('active');
+//     }
+//   }
+// }
+// ================================= end::scroll reveal =============================
+
